0bf283e3432dca9169e3db52c4c48dfa9ebde8f6
Divanshu Agrawal <109266857+dikanu@users.noreply.github.com>
2023-09-09 08:18:14 +0530

    Create store_data.php

---
+++ store_data.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+
+$sql = "INSERT INTO users (username, password, email) VALUES ('$username', '$password', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Data inserted successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
